5402d1165b5be367270edf4321816ca076446862
Evgeniy <Dgef2008@yandex.ru>
2020-04-01 17:25:56 +0300

    Эксперемент с препроцессором css
    Из вебинара "Час земли"

--- games/duck_hunt.php
+++ games/duck_hunt.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>Личный сайт студента GeekBrains</title>
     <link rel="stylesheet" href="../style.css" media="screen">
-    <link rel="stylesheet" href="../games.css" media="screen">
+    <link rel="stylesheet" href="../games.scss" media="screen">
 </head>
 
 <body>
@@ -22,7 +22,7 @@
     <main2>
         <div class="box_games">
             <h1>Игра "Охота на уток"</h1>
-            <p id="info">Отстреливайте щелчком мышки летящих в небе уточек. Всего 4 утки.</p><br>
+            <p id="info">Отстреливайте щелчком мышки летящих в небе уточек. Всего 9 утки.</p><br>
             <div class="map">
 
                 <input id="dog" type="name" placeholder="Не стреляй в собаку">
@@ -35,10 +35,29 @@
                 <label class="duck duck2 riffle" for="duck2"></label>
 
                 <input id="duck3" type="checkbox" class="riffle">
-                <label class="duck duck3 riffle" for="duck3"></label>
-
+                <label class="duck duck3 riffle" for="duck3"></label>                
+                
                 <input id="duck4" type="checkbox" class="riffle">
                 <label class="duck duck4 riffle" for="duck4"></label>
+
+                   
+                <input id="duck5" type="checkbox" class="riffle">
+                <label class="duck duck1 riffle" for="duck5"></label>
+
+                   
+                <input id="duck6" type="checkbox" class="riffle">
+                <label class="duck duck2 riffle" for="duck6"></label>
+
+                   
+                <input id="duck7" type="checkbox" class="riffle">
+                <label class="duck duck3 riffle" for="duck7"></label>
+
+                   
+                <input id="duck8" type="checkbox" class="riffle">
+                <label class="duck duck4 riffle" for="duck8"></label>
+
+                <input id="duck9" type="checkbox" class="riffle">
+                <label class="duck duck1 riffle" for="duck9"></label>
                 <div class="count"></div>
 
             </div>
